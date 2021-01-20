eb8fc97fe3c050adf9b8da7b30328f807c781773
donal <donalspring@gmail.com>
2021-01-20 18:25:57 +0000

    🙈 FIX - new foreveer logic 😱

--- forever.sh
+++ forever.sh
@@ -2,18 +2,20 @@
 
 
 if [ -z ${1} ]; then
-  echo "🤥 No Python3 script found - please pass one 🤥 "
+  echo "🤥 No Mac Address or Port found - please pass them 🤥 "
   exit 1
 fi
 
-SCRIPT_NAME=${1:=miflora-exporter.py}
+SCRIPT_NAME="miflora-exporter.py"
+MAC_ADDRESS=${1:="C4:7C:8D:6C:3E:8C"}
+PORT=${2:="6000"}
 
 run(){
-  echo "🏃‍♂️ Running ${SCRIPT_NAME}"
-  python3 -u ${SCRIPT_NAME}
+  echo "🏃‍♂️ Running ${SCRIPT_NAME} ${MAC_ADDRESS} ${PORT}"
+  python3 -u ${SCRIPT_NAME} ${MAC_ADDRESS} ${PORT}
 }
 
 until run; do
-  echo "${SCRIPT_NAME} crashed with exit code $?. Restarting..." >&2
+  echo "${SCRIPT_NAME} ${MAC_ADDRESS} ${PORT} crashed with exit code $?. Restarting..." >&2
   sleep 2
 done
--- miflora-exporter.py
+++ miflora-exporter.py
@@ -1,6 +1,7 @@
 from prometheus_client import start_http_server, Gauge
 import random
 import time
+import sys
 from datetime import datetime
 from btlewrap import BluepyBackend
 
@@ -14,6 +15,9 @@ from miflora.miflora_poller import (
     MiFloraPoller,
 )
 
+MAC_ADDRESS = sys.argv[1]
+PORT = sys.argv[2]
+
 # things to track
 # gTemperature = Gauge('temperature', 'Temperature by the plant')
 # gMoisture = Gauge('moisture', 'moisture by the plant')
@@ -22,13 +26,19 @@ from miflora.miflora_poller import (
 # gBattery = Gauge('battery', 'light by the plant')
 
 # sensor 1 = C4:7C:8D:6B:D6:51
+# sensor 2 = C4:7C:8D:6C:3E:8C
+# sensor 3 =
+# sensor 4 =
+# sensor 5 =
+# sensor 6 =
+
 SENSOR = Gauge('miflora_primary', 'Sensor Data from MiFlora plant sensor', [
                'resource_type'])
 
 # update metrics every 15s
 UPDATE_PERIOD = 15
 
-print("Starting miflora exporter")
+print("Starting miflora exporter %s on %s" % (MAC_ADDRESS, PORT))
 dateTimeObj = datetime.now()
 timestampStr = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
 print('Current Timestamp : ', timestampStr)
@@ -36,7 +46,7 @@ print('Current Timestamp : ', timestampStr)
 
 def process_sensor():
     """Poll data from the sensor."""
-    poller = MiFloraPoller("C4:7C:8D:6B:D6:51", BluepyBackend)
+    poller = MiFloraPoller(MAC_ADDRESS, BluepyBackend)
     # print("Getting data from Mi Flora")
 
     SENSOR.labels('temperature').set(poller.parameter_value(MI_TEMPERATURE))
@@ -45,9 +55,10 @@ def process_sensor():
     SENSOR.labels('conductivity').set(poller.parameter_value(MI_CONDUCTIVITY))
     SENSOR.labels('battery').set(poller.parameter_value(MI_BATTERY))
 
+
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
-    start_http_server(6000)
+    start_http_server(PORT)
     # Generate some requests.
     while True:
         process_sensor()
