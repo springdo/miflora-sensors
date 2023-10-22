0e99940392ac84f898fadf160722ea8c0440b0b4
Donal Spring <donalspring@gmail.com>
2023-10-22 20:07:44 +0100

    🦘 FIX - wrong ports / machines 🦘

--- all-forevers.sh
+++ all-forevers.sh
@@ -3,6 +3,7 @@
 
 # Label    =  Mac Address
 # -------- = ---------------
+
 # sensor A = C4:7C:8D:6B:D6:51 - not mine anymore :P
 # sensor B = C4:7C:8D:6C:3A:53
 # sensor C = C4:7C:8D:6C:3E:8C
@@ -19,21 +20,20 @@
 # sensor K = C4:7C:8D:6C:59:DC
 
 
-# livingroompi
+# livingroompi [B / C / D / G]
 nohup ./forever.sh C4:7C:8D:6C:3A:53 6000 >> /dev/null &
 nohup ./forever.sh C4:7C:8D:6C:3E:8C 6001 >> /dev/null &
 nohup ./forever.sh C4:7C:8D:6C:39:25 6002 >> /dev/null &
 nohup ./forever.sh C4:7C:8D:6C:57:61 6003 >> /dev/null &
 
-# bedroompi
-nohup ./forever.sh C4:7C:8D:6C:3E:06 6011 >> exporter.log &
-nohup ./forever.sh C4:7C:8D:6C:3E:76 6012 >> exporter.log &
+# bedroompi [k]
+nohup ./forever.sh C4:7C:8D:6C:59:DC 6011 >> exporter.log &
 
-# babyroompi
+# babyroompi [E / F / I]
 nohup ./forever.sh C4:7C:8D:6C:3E:06 6020 >> /dev/null &
 nohup ./forever.sh C4:7C:8D:6C:3E:76 6021 >> /dev/null &
-nohup ./forever.sh C4:7C:8D:6C:57:CE 6022 >> /dev/null &
+nohup ./forever.sh C4:7C:8D:6C:53:33 6022 >> /dev/null &
 
-# spareroompi
-nohup ./forever.sh C4:7C:8D:6C:56:E5 6030 >> exporter.log &
-nohup ./forever.sh C4:7C:8D:6C:59:DC 6031 >> exporter.log &
+# spareroompi [H / J]
+nohup ./forever.sh C4:7C:8D:6C:57:CE 6030 >> exporter.log &
+nohup ./forever.sh C4:7C:8D:6C:56:E5 6031 >> exporter.log &
